6a014e89824087719d422d8fc656d9f3a39042fd
divinize <patric_emmel@outlook.com>
2018-11-14 10:24:27 +0700

    [ADD] pojo Product

---
+++ src/main/java/com/behawk/powertrain/model/Product.java
@@ -0,0 +1,72 @@
+package com.behawk.powertrain.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.databind.ser.std.SerializableSerializer;
+
+
+
+@Entity
+public class Product extends SerializableSerializer{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    @NotBlank
+    private String productName;
+
+    private String description;
+
+    @NotBlank
+    private double price;
+
+    @NotBlank
+    private int view;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return this.productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getView() {
+        return this.view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
+    
+}
