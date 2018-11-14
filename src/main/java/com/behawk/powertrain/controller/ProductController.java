78f2cf80d283e4d369f734ead23201142eb49efd
divinize <patric_emmel@outlook.com>
2018-11-14 11:01:09 +0700

    [ADD]Product Controller

---
+++ src/main/java/com/behawk/powertrain/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.behawk.powertrain.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import com.behawk.powertrain.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public ResponseEntity<Product> getAllProduct(){
+        return null;
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProductByCampaignName(
+        @RequestParam("campaign_name") String targetCampaignName,
+        @RequestParam("sortBy") String sortType,
+        @RequestParam("pageNumber")int paging){
+            return null;
+    }
+
+    @GetMapping("/products/bestSell")
+    public ResponseEntity<List<Product>> getBestSellProduct(){
+        return null;
+    }
+
+
+}
