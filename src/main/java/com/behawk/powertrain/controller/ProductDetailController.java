c7ac39e0417a81f037ea40b34324f9c0a4c01184
Unknown <nattapat27@gmail.com>
2018-11-14 14:55:12 +0700

    [ADD] Product Detail

---
+++ src/main/java/com/behawk/powertrain/controller/ProductDetailController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.behawk.powertrain.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author benz
+ */
+@RestController
+@RequestMapping("/product-detail")
+public class ProductDetailController {
+    
+}
