7f182cad2918ba4533437533c626165e9a5c7bbd
FlookSupakorn <supakorn.flook@mail.kmutt.ac.th>
2018-11-18 19:53:17 +0700

    [ADD] UpdateOrderDetailQty Method

--- src/main/java/com/behawk/powertrain/controller/OrderDetailController.java
+++ src/main/java/com/behawk/powertrain/controller/OrderDetailController.java
@@ -5,6 +5,12 @@
  */
 package com.behawk.powertrain.controller;
 
+import com.behawk.powertrain.model.OrderDetail;
+import com.behawk.powertrain.service.OrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,4 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class OrderDetailController {
 
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @PatchMapping("/cart/orderdetail/{orderDetailId}")
+    public OrderDetail updateOrderDetailQty(@PathVariable long orderDetailId, @RequestBody(required = false) OrderDetail orderDetailBody) {
+        int orderDetailQty = orderDetailBody.getQuantity();
+        System.out.println("");
+        OrderDetail orderDetail = orderDetailService.UpdateOrderDetailQty(orderDetailId, orderDetailQty);
+        return orderDetail;
+    }
 }
--- src/main/java/com/behawk/powertrain/service/OrderDetailService.java
+++ src/main/java/com/behawk/powertrain/service/OrderDetailService.java
@@ -5,6 +5,9 @@
  */
 package com.behawk.powertrain.service;
 
+import com.behawk.powertrain.Repository.OrderDetailRepository;
+import com.behawk.powertrain.model.OrderDetail;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,4 +17,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderDetailService {
 
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    public OrderDetail UpdateOrderDetailQty(long orderDetailId, int orderDetailQty) {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail = orderDetailRepository.getOne(orderDetailId);
+        orderDetail.setQuantity(orderDetailQty);
+        orderDetailRepository.save(orderDetail);
+        return orderDetail;
+    }
 }
