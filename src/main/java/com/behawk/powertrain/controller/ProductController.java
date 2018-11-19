32ed17a21d40b900f861657e2bb7fb18fd27e876
Pataranan Wutthiprapakit <sindy_love_2540@hotmail.com>
2018-11-19 15:31:43 +0700

    [ADD] deleteProduct

--- src/main/java/com/behawk/powertrain/controller/ProductController.java
+++ src/main/java/com/behawk/powertrain/controller/ProductController.java
@@ -8,6 +8,7 @@ import com.behawk.powertrain.model.Product;
 import com.behawk.powertrain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,10 @@ public class ProductController {
         return productService.updateProduct(product);
     }
     
+    @DeleteMapping("/roduct/product{id}")
+    public Product deleteProduct(@RequestBody Product product){
+        return productService.updateProduct(product);
+    }
 }
 
 
