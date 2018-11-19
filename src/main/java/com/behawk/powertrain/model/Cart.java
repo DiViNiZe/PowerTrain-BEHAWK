e2baea29537e028a747030b9591ec8a1624b7d4b
divinize <patric_emmel@outlook.com>
2018-11-19 16:26:40 +0700

    [ADD] cart pojo

--- src/main/java/com/behawk/powertrain/model/Cart.java
+++ src/main/java/com/behawk/powertrain/model/Cart.java
@@ -3,9 +3,11 @@ package com.behawk.powertrain.model;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.databind.ser.std.SerializableSerializer;
@@ -20,4 +22,8 @@ public class Cart extends SerializableSerializer{
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 
-    
+    @OneToOne(fetch=FetchType.EAGER)
+    private Order order;
+
+}
+
