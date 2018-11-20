782446d2ed85f771fc1c57e338f1e2749f0c601f
Pataranan Wutthiprapakit <sindy_love_2540@hotmail.com>
2018-11-20 14:56:06 +0700

    [UPDATE] paymentBean

--- src/main/java/com/behawk/powertrain/bean/paymentBean.java
+++ src/main/java/com/behawk/powertrain/bean/paymentBean.java
@@ -5,10 +5,22 @@
  */
 package com.behawk.powertrain.bean;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  *
  * @author Sinn
  */
+@Configuration
 public class paymentBean {
+    
+    @Value("${secretkey}")
+    private String SECRET_KEY;
+    
+    @Value("${publickey}")
+    private String PUBLIC_KEY;
     
 }
