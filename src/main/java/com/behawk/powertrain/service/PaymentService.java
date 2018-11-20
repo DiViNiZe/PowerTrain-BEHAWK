4f38c704d35ad4bfba44ac7a65bf41ea50b7c65a
Unknown <nattapat27@gmail.com>
2018-11-20 16:30:40 +0700

    [ADD] pay with omise

--- src/main/java/com/behawk/powertrain/service/PaymentService.java
+++ src/main/java/com/behawk/powertrain/service/PaymentService.java
@@ -2,8 +2,13 @@ package com.behawk.powertrain.service;
 
 import co.omise.Client;
 import co.omise.ClientException;
+import co.omise.models.Charge;
+import com.behawk.powertrain.Repository.OrderRepository;
 import com.behawk.powertrain.Repository.PaymentRepository;
+import com.behawk.powertrain.model.Order;
+import com.behawk.powertrain.model.OrderDetail;
 import com.behawk.powertrain.model.Payment;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +22,10 @@ public class PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
     private Client client;
+    
+    @Autowired
+    private OrderRepository orderRepository;
+    
     public PaymentService(String publicKey, String secretKey) throws ClientException {
         client = new Client(publicKey, secretKey);
     }
@@ -26,7 +35,24 @@ public class PaymentService {
     }
     
     public boolean confirmPayment(String paymentToken, long orderId){
+        Order order = orderRepository.findOrderByOrderId(orderId);
+        List<OrderDetail> detail = order.getOrderDetail();
+        double money = 0;
+        for(OrderDetail od : detail){
+            money+=od.getTotalPrice();
+        }
+        try{
+            Charge charge = client.charges().create(
+                                new Charge.Create()
+                                        .amount((long) money)
+                                        .currency("THB")
+                                        .card(paymentToken)
+                            );
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         return false;
     }
-    
 }
